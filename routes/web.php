83dd85a314c97ac3136dfe37f28a287d5831348b
tiktanika000 <tik.tanika.m@mail.ukrim.ac.id>
2024-03-03 16:14:58 +0700

    file project

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,13 @@ Route::group(['middleware' => 'auth:user'],function (){
     Route::prefix('admin')->group(function (){
         Route::get('/',[\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard.index');
         Route::get('/profile',[\App\Http\Controllers\DashboardController::class,'profile'])->name('dashboard.profile');
+
+        Route::get('/kategori',[\App\Http\Controllers\KategoriKontroller::class, 'index'])->name('kategori.index');
+        Route::get('/kategori/tambah',[\App\Http\Controllers\KategoriKontroller::class, 'tambah'])->name('kategori.tambah');
+        Route::post('/kategori/prosesTambah',[\App\Http\Controllers\KategoriKontroller::class, 'prosesTambah'])->name('kategori.prosesTambah');
+        Route::get('/kategori/ubah/{id}',[\App\Http\Controllers\KategoriKontroller::class, 'ubah'])->name('kategori.ubah');
+        Route::post('/kategori/prosesUbah',[\App\Http\Controllers\KategoriKontroller::class, 'prosesUbah'])->name('kategori.prosesUbah');
+        Route::get('/kategori/hapus/{id}',[\App\Http\Controllers\KategoriKontroller::class, 'hapus'])->name('kategori.hapus');
     });
     Route::get('/logout',[\App\Http\Controllers\AuthController::class,'logout'])->name('auth.logout');
 });
